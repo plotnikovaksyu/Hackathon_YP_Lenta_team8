0b22dec7fc2c16da9bf5e4160b948cfb5f083507
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-10-08 13:35:05 +0300

    feat: финальная версия 1 графика

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -21,29 +21,62 @@ ChartJS.register(
 
 function Chart() {
 
-    let day = new Date().toLocaleDateString()
-
+    // let day = new Date().toLocaleDateString()
+    let day = new Date().toLocaleDateString().slice(0, 5)
 
     const data = {
         labels: [day, day, day, day, day, day, day, day, day, day, day, day, day, day],
         datasets: [
             {
-                label: 'Sales the week',
-                data: [6, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9],
-                backgroundColor: 'aqua',
-                borderColor: 'black',
+                label: 'Булочка с маком',
+                data: [6, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 23],
+                backgroundColor: '#6E237D',
+                borderColor: '#6E237D',
                 pointBorderColor: 'aqua',
                 fill: true,
-                tension: 0.4
+                tension: 0.4,
+                radius: 4,
+
             },
             {
-                label: 'Sales the week2',
-                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2],
+                label: 'Булочка с повидлом',
+                data: [8, 5, 22, 43, 26, 49, 34, 4, 5, 50, 31, 12, 23, 25],
                 backgroundColor: 'blue',
                 borderColor: 'blue',
                 pointBorderColor: 'aqua',
                 fill: true,
-                tension: 0.4
+                tension: 0.4,
+                radius: 4,
+            },
+            {
+                label: 'Булочка со сгущенкой',
+                data: [11, 22, 13, 4, 15, 26, 37, 48, 19, 13, 12, 11, 2, 23],
+                backgroundColor: '#AA2332',
+                borderColor: '#AA2332',
+                pointBorderColor: 'aqua',
+                fill: true,
+                tension: 0.4,
+                radius: 4,
+            },
+            {
+                label: 'Булочка со помадкой',
+                data: [12, 2, 31, 43, 5, 16, 17, 8, 9, 17, 18, 14, 21, 48],
+                backgroundColor: '#2FCBFF',
+                borderColor: '#2FCBFF',
+                pointBorderColor: 'aqua',
+                fill: true,
+                tension: 0.4,
+                radius: 4,
+            },
+            {
+                label: 'Булочка с пудрой',
+                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2],
+                backgroundColor: '#00BE64',
+                borderColor: '#00BE64',
+                pointBorderColor: 'aqua',
+                fill: true,
+                tension: 0.4,
+                radius: 4,
             },
 
         ],
@@ -54,23 +87,43 @@ function Chart() {
             legend: {
                 display: true,
                 position: "bottom",
-                align: "start"
+                align: "start",
+                labels: {
+                    boxWidth: 8,
+                    boxHeight: 8,
+                    useBorderRadius: true,
+                    borderRadius: 8,
+                }
             }
         },
         scales: {
             y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Прогноз спроса, шт',
+                    position: 'top',
+                    align: 'end',
+                    font: {
+                        size: 18,
+                    }
 
+                }
             }
         },
+        responsive: true,
+        maintainAspectRatio: false
     }
 
     return (
         <div className="chart">
-            <Line
-                data={data}
-                options={options}>
-            </Line>
-        </div>
+            <div className='chart__container'>
+                <Line
+                    data={data}
+                    options={options}>
+                </Line>
+            </div>
+        </div >
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import { useLocation } from "react-router-dom";
-import Backup from "../../images/Backup.svg"
+import Backup from "../../images/Backup.svg";
+import SearchIcon from "../../images/SearchIcon.svg"
 
 function Header() {
     let date = new Date()
@@ -14,6 +15,9 @@ function Header() {
                 <p className='header__text' >{location.pathname === '/1screen' ? 'Прогноз спроса' : "Статистика"}</p>
                 <img className="header__image" src={Backup} />
                 <div className='header__data'>{day}:{minutes} обновлен сегодня </div>
+            </div>
+            <div className="header__search_container">
+                <img className="header__search" src={SearchIcon} />
             </div>
         </header>
     )
