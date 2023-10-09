067d9c629d23c85c301d0428849eb4cd43c5d21c
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2023-10-10 00:26:17 +0300

    fix: добавить basename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Hackathon_YP_Lenta_team8/',
   plugins: [react()],
 })
